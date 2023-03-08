b40664b282d94c14dfd228ca1a49eb383b6dd520
Ali Ahammad <68907011+li812@users.noreply.github.com>
2023-03-08 14:11:55 +0530

    main

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='lisql',
+    version='2.0.1',
+    py_modules=['lisql'],
+    description='Provides simple funtions to interact with MySQL databases and tables.',
+    author='Ali Ahammad',
+    author_email='aliahammad0812@outlook.com',
+    url='https://github.com/li812/lisql',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
